63863a44e09f55bb8d77459f6ec1ebb3a318663f
James S <jamesnshiffer@gmail.com>
2020-06-18 22:01:07 +0000

    added combat!

--- commands.js
+++ commands.js
@@ -3,129 +3,171 @@ var allAnimals = require('./animals.json');
 const helpers = require('./helpers.js');
 const process = require('process');
 
-module.exports = {
-    craft: function (player, args) {
-        let itemName = args[1];
-        if (!itemName) {
-            console.error('You did not specify an item to craft.');
-            return helpers.Status.FAILURE;
-        }
-        let item = allItems[itemName];
-        // only allow already discovered items with ingredients list to be crafted 
-        if (item && item.ingredients && item.discovered) {
-            let success = helpers.removeItems(player.inventory, item.ingredients);
-            if (success) {
-                player.inventory.push({ name: itemName });
-                console.log(`You crafted a ${itemName}!`);
-                return helpers.status.SUCCESS;
-            } else {
-                console.log(`To craft a ${itemName} you need:`);
-                for (let ingredient in item.ingredients) {
-                    let requiredCount = item.ingredients[ingredient];
-                    console.log(`- ${requiredCount}x ${ingredient}`);
-                }
-                return helpers.Status.FAILURE;
-            }
+const craft = function (player, args) {
+    let itemName = args[1];
+    if (!itemName) {
+        console.error('You did not specify an item to craft.');
+        return helpers.Status.FAILURE;
+    }
+    let item = allItems[itemName];
+    // only allow already discovered items with ingredients list to be crafted 
+    if (item && item.ingredients && item.discovered) {
+        let success = helpers.removeItems(player.inventory, item.ingredients);
+        if (success) {
+            player.inventory.push({ name: itemName });
+            console.log(`You crafted a ${itemName}!`);
+            return helpers.status.SUCCESS;
         } else {
-            console.log('You\'ve never heard of that before.');
-            return helpers.Status.FAILURE;
-        }
-    },
-    eat: function (player, args) {
-        if (player.hunger < 100) {
-            let success = helpers.removeItems(player.inventory, {'Rations': 1});
-            if (success) {
-                player.hunger += helpers.randomInt(...allItems.Rations.hunger);
-                player.hunger = Math.min(player.hunger, 100);
-                return helpers.Status.SUCCESS;
-            } else {
-                console.error('You have nothing to eat.');
-                return helpers.Status.FAILURE;
+            console.log(`To craft a ${itemName} you need:`);
+            for (let ingredient in item.ingredients) {
+                let requiredCount = item.ingredients[ingredient];
+                console.log(`- ${requiredCount}x ${ingredient}`);
             }
-        } else {
-            console.error('You are not hungry.');
             return helpers.Status.FAILURE;
         }
-    },
-    walk: function (player, args) {
-        const place = args[1] || 'middle of nowhere';
-        console.log(`You walk to the ${place}.`);
-        player.hunger -= helpers.randomInt(1, 5);
-        player.justMoved = true;
-        return helpers.Status.SUCCESS | helpers.Status.MOVED;
-    },
-    run: function (player, args) {
-        const place = args[1] || 'middle of nowhere';
-        console.log(`You run to the ${place}.`);
-        player.hunger -= helpers.randomInt(3, 10);
-        player.justMoved = true;
-        return helpers.Status.SUCCESS | helpers.Status.MOVED;
-    },
-    inventory: function (player) {
-        console.log(`${player.inventory.length}/${player.maxInventorySlots} items full`);
-        if (player.inventory.length === 0) {
-            console.log('You have nothing!');
-            return helpers.Status.FAILURE;
-        }
-        Object.keys(allItems).forEach(item => {
-            const count = helpers.countItem(player.inventory, item);
-            if (count > 0) {
-                console.log(`${item} x${count}`);
-            }
-        });
-        return helpers.Status.SUCCESS;
-    },
-    rummage: function (player) {
-        if (player.inventory.length === player.maxInventorySlots) {
-            console.error('Your inventory is full! Please drop something first.');
-            return helpers.Status.FAILURE;
-        }
-        const randItem = helpers.randomItem();
-        player.hunger -= helpers.randomInt(3, 5);
-        if (randItem === 'Nothing') {
-            console.log('You didn\'t pick up anything!');
-        } else {
-            if (!allItems[randItem].hidden) {
-                player.inventory.push({ name: randItem });
-            }
-            console.log(`You picked up a ${randItem}!`);
-            if (randItem === 'Backpack') {
-                player.maxInventorySlots += 30;
-                console.log(`Your inventory can now hold ${player.maxInventorySlots} items.`);
-            }
-            allItems[randItem].discovered = true;
-        }
-        return helpers.Status.SUCCESS | helpers.Status.MOVED;
-    },
-    drop: function (player, args) {
-        const itemName = args[1];
-        if (!itemName) {
-            console.error('You did not specify an item to drop.');
-            return helpers.Status.FAILURE;
-        }
-        const result = helpers.removeItems(player.inventory, {
-            [itemName]: 1
-        });
-        if (result) {
-            console.log(`You dropped the ${itemName}.`);
+    } else {
+        console.log('You\'ve never heard of that before.');
+        return helpers.Status.FAILURE;
+    }
+};
+
+const eat = function (player, args) {
+    if (player.hunger < 100) {
+        let success = helpers.removeItems(player.inventory, {'Rations': 1});
+        if (success) {
+            player.hunger += helpers.randomInt(...allItems.Rations.hunger);
+            player.hunger = Math.min(player.hunger, 100);
             return helpers.Status.SUCCESS;
         } else {
-            console.log(`Failed to drop a ${itemName}.`);
+            console.error('You have nothing to eat.');
             return helpers.Status.FAILURE;
         }
-    },
-    clear: function () {
-        console.clear();
-        return helpers.Status.SUCCESS;
-    },
-    help: function () {
-        console.log(
-            Object.keys(module.exports).join(', ')
-        );
-        return helpers.Status.SUCCESS;
-    },
-    quit: function () {
-        process.exit(0);
-        return helpers.Status.SUCCESS;
+    } else {
+        console.error('You are not hungry.');
+        return helpers.Status.FAILURE;
     }
+};
+
+const walk = function (player, args) {
+    const place = args[1] || 'middle of nowhere';
+    console.log(`You walk to the ${place}.`);
+    player.hunger -= helpers.randomInt(1, 5);
+    player.justMoved = true;
+    return helpers.Status.SUCCESS | helpers.Status.MOVED;
+};
+
+const run = function (player, args) {
+    const place = args[1] || 'middle of nowhere';
+    console.log(`You run to the ${place}.`);
+    player.hunger -= helpers.randomInt(3, 10);
+    player.justMoved = true;
+    return helpers.Status.SUCCESS | helpers.Status.MOVED;
+};
+
+const inventory = function (player) {
+    console.log(`${player.inventory.length}/${player.maxInventorySlots} items full`);
+    if (player.inventory.length === 0) {
+        console.log('You have nothing!');
+        return helpers.Status.FAILURE;
+    }
+    Object.keys(allItems).forEach(item => {
+        const count = helpers.countItem(player.inventory, item);
+        if (count > 0) {
+            console.log(`${item} x${count}`);
+        }
+    });
+    return helpers.Status.SUCCESS;
+};
+
+const rummage = function (player) {
+    if (player.inventory.length === player.maxInventorySlots) {
+        console.error('Your inventory is full! Please drop something first.');
+        return helpers.Status.FAILURE;
+    }
+    const randItem = helpers.randomItem();
+    player.hunger -= helpers.randomInt(3, 5);
+    if (randItem === 'Nothing') {
+        console.log('You didn\'t pick up anything!');
+    } else {
+        if (!allItems[randItem].hidden) {
+            player.inventory.push({ name: randItem });
+        }
+        console.log(`You picked up a ${randItem}!`);
+        if (randItem === 'Backpack') {
+            player.maxInventorySlots += 30;
+            console.log(`Your inventory can now hold ${player.maxInventorySlots} items.`);
+        }
+        allItems[randItem].discovered = true;
+    }
+    return helpers.Status.SUCCESS | helpers.Status.MOVED;
+};
+
+const attack = function (player, args) {
+    // make sure the player is in combat
+    if (!player.opponent) {
+        console.error('You are not fighting anything.');
+        return helpers.Status.FAILURE;
+    }
+    let itemName = args[1];
+    if (itemName) {
+        // check that you have the weapon you want to attack with
+        if (itemName === 'Bow') itemName = 'Arrow';
+        if (helpers.countItem(player.inventory, itemName) === 0) {
+            console.error('You don\'t have that weapon.');
+            return helpers.Status.FAILURE;
+        }
+    } else {
+        itemName = 'Fists';
+    }
+    let weapon = allItems[itemName];
+    let dmg = helpers.randomInt(...weapon.strength);
+    player.opponent.health -= dmg;
+    let move = helpers.randomChoice(weapon.moves);
+    console.log(`=== You ${move} the ${player.opponent.name}, doing ${dmg} HP of damage ===`);
+    // if the player shot an arrow, remove it from the inventory
+    if (itemName === 'Arrow') {
+        helpers.removeItems(player.inventory, { Arrow: 1 });
+    }
+    return helpers.Status.SUCCESS;
+};
+
+const drop = function (player, args) {
+    const itemName = args[1];
+    if (!itemName) {
+        console.error('You did not specify an item to drop.');
+        return helpers.Status.FAILURE;
+    }
+    const result = helpers.removeItems(player.inventory, {
+        [itemName]: 1
+    });
+    if (result) {
+        console.log(`You dropped the ${itemName}.`);
+        return helpers.Status.SUCCESS;
+    } else {
+        console.log(`Failed to drop a ${itemName}.`);
+        return helpers.Status.FAILURE;
+    }
+};
+
+const clear = function () {
+    console.clear();
+    return helpers.Status.SUCCESS;
+};
+
+const help = function () {
+    console.log(
+        Object.keys(module.exports).join(', ')
+    );
+    return helpers.Status.SUCCESS;
+};
+
+const quit = function () {
+    process.exit(0);
+    return helpers.Status.SUCCESS;
+};
+
+module.exports = {
+    craft, eat, walk, run, attack,
+    inventory, rummage, drop,
+    clear, help, quit
 };
--- index.js
+++ index.js
@@ -18,21 +18,29 @@ console.log(`Hello, ${player.name}!`);
 
 
 /**
- * Prints out the status of the player. Should run every turn.
+ * Prints out the status of the player and opponent, if there is one. Should run every turn.
  * @returns undefined
  */
 function printStatus()
 {
     console.log();
+    if (player.opponent) {
+        console.log(`=== ${player.opponent.name}: ${player.opponent.health}/${player.opponent.maxHealth} HP ===`);
+    }
     console.log(`Health: ${player.health}/100, Hunger: ${player.hunger}/100`);
 }
 
 /**
- * Checks whether the player has died. Should print out a death message.
+ * Checks whether the player (or opponent, if there is one) has died. Should print out a death message.
  * @returns bool
  */
 function checkDeath()
 {
+    // check if opponent has died, and remove it if it has
+    if (player.opponent && player.opponent.health <= 0) {
+        console.log(`=== The ${player.opponent.name} died! ===`);
+        player.opponent = null;
+    }
     if (player.hunger <= 0) {
         console.log('You starved to death');
         return true;
@@ -62,9 +70,8 @@ function doCombatTurn()
     } else {
         // attack
         let randMove = helpers.randomChoice(player.opponent.moves);
-        console.log(`=== The ${player.opponent.name} ${randMove}! ===`);
         let dmg = helpers.randomInt(...player.opponent.strength);
-        console.log(`=== That did ${dmg} HP of damage! ===`);
+        console.log(`=== The ${player.opponent.name} ${randMove}, doing ${dmg} HP of damage! ===`);
         player.health -= dmg;
     }
 }
